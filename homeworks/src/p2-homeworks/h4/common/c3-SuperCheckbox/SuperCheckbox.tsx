462b98649f1e27e1805d6dd43ddc30c5bdaa4c9e
Dudko Kate <ekaterina_dr@tut.by>
2022-03-30 10:22:16 +0300

    fixed CSS from Checkbox in homework 4

--- homeworks/src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ homeworks/src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -4,14 +4,14 @@ import s from './SuperCheckbox.module.css'
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-type SuperCheckboxPropsType = DefaultInputPropsType & {
+type SuperCheckboxPropsType = Exclude<DefaultInputPropsType, "type"> & {
     onChangeChecked?: (checked: boolean) => void
     spanClassName?: string
 }
 
 const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        // type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
         onChange, onChangeChecked,
         className = s.label, spanClassName,
         children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
@@ -20,6 +20,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
           onChangeChecked && onChangeChecked(e.currentTarget.checked)
         // сделайте так чтоб работал onChange и onChangeChecked
     }
